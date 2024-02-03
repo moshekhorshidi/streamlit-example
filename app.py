5205a79b8ea617aa5b7d0eb5c5f1c1d8b67445e9
Moshe Khorshidi Code Jungle <moshekhorshidi@gmail.com>
2024-02-04 01:28:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,19 +7,6 @@ import time
 import streamlit as st
 
 
-with st.echo():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
-
-    @st.experimental_singleton
-    def get_driver():
-        return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-    options = Options()
-    options.add_argument('--disable-gpu')
-    options.add_argument('--headless')
 
 st.title("New York City Zola Plan Search")
 
